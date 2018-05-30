7784743d6a597fba22e83326b77553acbf1462e5
Cooper Bell <john.cooper.bell@gmail.com>
2018-05-29 21:32:06 -0500

    Finish error handling

--- parsing.c
+++ parsing.c
@@ -74,41 +74,42 @@ void lval_print(lval v) {
 
 void lval_println(lval v) { lval_print(v); putchar('\n'); }
 
-long eval_op(lval x, char* op, lval y) {
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
-  
-	
-  if (strcmp(op, "+") == 0) { lval_num(return x.num + y.num); }
-  if (strcmp(op, "-") == 0) { lval_num(return x.num - y.num); }
-  if (strcmp(op, "*") == 0) { lval_num(return x.num * y.num); }
-  if (strcmp(op, "/") == 0) { 
-    return y == 0 
+lval eval_op(lval x, char* op, lval y) {
+  if (x.type == LVAL_ERR) { return lval_num(x.num); }
+  if (y.type == LVAL_ERR) { return lval_num(y.num); }
+
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+  if (strcmp(op, "/") == 0) {
+    return y.num == 0
       ? lval_err(LERR_DIV_ZERO)
-      : lval_num(x.num / y.num); 
+      : lval_num(x.num / y.num);
   }
   if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
   if (strcmp(op, "^") == 0) { return lval_num(pow(x.num, y.num)); }
   if (strcmp(op, "min") == 0) { return lval_num(x.num < y.num ? x.num : y.num); }
   if (strcmp(op, "max") == 0) { return lval_num(x.num < y.num ? y.num : x.num); }
-  
+
   return lval_err(LERR_BAD_OP);
 }
 
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   char* op = t->children[1]->contents;
-  
-  long x = eval(t->children[2]);
+  lval x = eval(t->children[2]);
+
   int i = 3;
   while (strstr(t->children[i]->tag, "expr")) {
     x = eval_op(x, op, eval(t->children[i]));
     i++;
   }
-  
+
   return x;
 }
 
@@ -118,8 +119,8 @@ int main(int argc, char** argv) {
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lispy    = mpc_new("lispy");
-  
-  //operator : '+' | '-' | '*' | '/' | '%' | '^' ;  
+
+  //operator : '+' | '-' | '*' | '/' | '%' | '^' ;
   mpca_lang(MPCA_LANG_DEFAULT,
     "						                           \
       number   : /-?([0-9]+\\.)?[0-9]+/ ;	                           \
@@ -128,26 +129,26 @@ int main(int argc, char** argv) {
       lispy    : /^/ <operator> <expr>+ /$/ ;                              \
     ",
     Number, Operator, Expr, Lispy);
-  
+
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+d to Exit\n");
 
   char* input;
   while((input = readline("λ> ")) != NULL) {
     add_history(input);
-    
+
     /* Parse user input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       //mpc_ast_print(r.output);
-      long result = eval(r.output);
-      printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-    
+
     free(input);
   }
 
